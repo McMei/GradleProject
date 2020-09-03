f7104eed01541590392552ad69a572d377a20e7d
McMei <y1131297010@icloud.com>
2020-09-03 19:11:46 -0400

    Week3 Controller update

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,4 +1,8 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class Controller {
@@ -6,8 +10,15 @@ public class Controller {
     @FXML
     private Label lblOutput;
 
-    public void sayHello() {
-        lblOutput.setText("Hello FXML!");
+    @FXML
+    private Button eventHandler;
+
+    @FXML
+    private Button RecordProduction;
+
+    @FXML
+    public void printConsole(ActionEvent event) {
+        System.out.println();
 
     }
 
